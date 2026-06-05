fb173dc9ba7fa49da742c7bcfc098b42bab15bb3
shrmax43 <r7605584@gmail.com>
2026-06-05 19:28:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/College-project-Retail-Sales/",
   server: {
     port: 5173,
-    proxy: {
-      "/api": "http://127.0.0.1:4000"
-    }
-  }
+  },
 });
